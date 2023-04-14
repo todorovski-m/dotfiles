aa167e4e914a73bd673b5d238833cd627e72a096
Mirko Todorovski <todorovski.mrk@gmail.com>
2023-04-14 13:20:48 +0200

    two monitors

---
+++ scripts/.config/scripts/two_monitors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $(hostname) = "dell-3567" ]
+then
+    xrandr --output HDMI-1 --auto --right-of eDP-1
+    xgamma -gamma 0.95
+    i3-msg "workspace 8, move workspace to output HDMI-1"
+    i3-msg "workspace 9, move workspace to output HDMI-1"
+    i3-msg "workspace 10, move workspace to output HDMI-1"
+    i3-msg "workspace 1"
+    bg=/home/mirko/Dropbox/wallpapers/pexels-james-wheeler-417074.jpg
+    feh --bg-fill $bg $bg
+fi
