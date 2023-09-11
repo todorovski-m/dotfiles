77764fe7fb55ec5c0f353bd1a4e8e4bb41b8c64b
Mirko Todorovski <todorovski.mrk@gmail.com>
2023-09-11 23:20:52 +0200

    change colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -7,8 +7,8 @@ vim.opt.termguicolors = true
 -- vim.cmd("colorscheme rose-pine")
 -- vim.cmd("colorscheme kanagawa")
 -- vim.cmd("colorscheme codemonkey")
-vim.cmd("colorscheme onedark")
--- vim.cmd("colorscheme onedarker")
+-- vim.cmd("colorscheme onedark")
+vim.cmd("colorscheme onedarker")
 vim.cmd("highlight clear SignColumn")
 vim.cmd("highlight clear LineNr")
 
