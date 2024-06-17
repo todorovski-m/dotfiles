b6c149d5d7c1231c16be5851d728a5323a685e8f
Mirko Todorovski <todorovski.mrk@gmail.com>
2024-06-17 13:09:39 +0200

    add colorscheme cyberdream

--- lazy_nvim/.config/lazy_nvim/lua/plugins/colorschemes.lua
+++ lazy_nvim/.config/lazy_nvim/lua/plugins/colorschemes.lua
@@ -22,4 +22,19 @@ return {
       })
     end,
   },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        -- Recommended - see "Configuring" below for more config options
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      })
+    end,
+  },
 }
