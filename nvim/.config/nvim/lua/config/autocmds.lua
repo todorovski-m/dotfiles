9a46bf661c4df5ac9c1308e8a022743b17509815
Mirko Todorovski <todorovski.mrk@gmail.com>
2025-03-18 23:19:10 +0100

    configure asyncrun

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,6 +7,8 @@ vim.cmd([[
 augroup filetype_py
     autocmd FileType python nnoremap <buffer> <silent> <F5> :update<CR> :TerminatorRunFileInOutputBuffer<CR>
     autocmd FileType python inoremap <buffer> <silent> <F5> <Esc>:update<CR> :TerminatorRunFileInOutputBuffer<CR>
+    autocmd FileType python nnoremap <buffer> <silent> <F6> :update<CR> :AsyncTask file-run<CR>
+    autocmd FileType python inoremap <buffer> <silent> <F6> <Esc>:update<CR> :AsyncTask file-run<CR>
 augroup end
 
 augroup filetype_sh
--- nvim/.config/nvim/lua/plugins/asyncrun.lua
+++ nvim/.config/nvim/lua/plugins/asyncrun.lua
@@ -1,4 +1,9 @@
+local function init_asyncrun()
+  vim.g["asyncrun_open"] = 10 -- quickfix window can be opened automatically
+end
+
 return {
   "skywind3000/asyncrun.vim",
+  init = init_asyncrun(),
   ft = { "fortran", "python" },
 }
