8ad24c14ef5fb90217c0937194fa653e6b944ce5
Mirko Todorovski <todorovski.mrk@gmail.com>
2025-04-06 23:07:29 +0200

    add libraries to path

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -6,9 +6,13 @@ export PATH="/usr/local/MATLAB/R2016b/bin:$PATH"
 export PATH="/usr/local/MATLAB/R2016b/bin/glnxa64:$PATH"
 export PATH="/usr/local/lib/nodejs/node-v12.16.3/bin:$PATH"
 export PATH="/home/mirko/.local/bin:$PATH"
+export LIBRARY_PATH=/usr/local/lib:$LIBRARY_PATH
+export FC=gfortran
 if [[ $(uname) == "Darwin" ]]; then
   export PATH=$PATH:/Applications/MEGAcmd.app/Contents/MacOS:/opt/homebrew/bin
   export PATH="/Volumes/MacApps/CPLEX_Studio_Community2211/cplex/bin/arm64_osx:$PATH"
+  export CPLUS_INCLUDE_PATH="/Users/mirko/Documents/eigen-3.4.0":$CPLUS_INCLUDE_PATH
+  export CPLUS_INCLUDE_PATH="/Users/mirko/Documents/nlohmann-json":$CPLUS_INCLUDE_PATH
 fi
 
 stty -ixon
