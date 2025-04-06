f033f789e0b0db63d4a0d5b531f5ed9daf9ea3fa
Mirko Todorovski <todorovski.mrk@gmail.com>
2025-04-06 23:07:06 +0200

    update asynctasks

--- nvim/.config/nvim/lua/plugins/asynctasks.lua
+++ nvim/.config/nvim/lua/plugins/asynctasks.lua
@@ -1,9 +1,6 @@
 return {
   "skywind3000/asynctasks.vim",
   init = function()
-    vim.g.asynctasks_extra_config = {
-      "~/github/my_dotfiles/my_tasks.ini",
-    }
     vim.g["asynctasks_config_name"] = "tasks.ini"
   end,
 }
