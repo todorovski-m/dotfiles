1da22c550c242cf2cc0b3bbe2224bccee6e15852
Mirko Todorovski <todorovski.mrk@gmail.com>
2025-12-30 09:57:51 +0100

    use underline for spell errors

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
 require("lspconfig").fortls.setup({})
+
+vim.api.nvim_set_hl(0, "SpellBad", {
+  undercurl = false,
+  underline = true,
+  sp = "#f7768e", -- underline color
+})
