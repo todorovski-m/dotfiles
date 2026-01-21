757a69b55f718cc2219d9e195f6b7c9883ebe1c8
Mirko Todorovski <todorovski.mrk@gmail.com>
2026-01-21 14:42:02 +0100

    remove ale from init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-require("lspconfig").fortls.setup({})
-
 require("config.functions")
 
 vim.api.nvim_set_hl(0, "SpellBad", {
