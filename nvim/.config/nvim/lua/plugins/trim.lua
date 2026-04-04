07ff1348b5f9c95324dea300c6ad9921e84b392b
Mirko Todorovski <todorovski.mrk@gmail.com>
2026-04-04 11:13:04 +0200

    Add auto-trim for trailing whitespace on save

--- nvim/.config/nvim/lua/plugins/trim.lua
+++ nvim/.config/nvim/lua/plugins/trim.lua
@@ -1,1 +1,14 @@
-return { "nvim-mini/mini.trailspace", version = false }
+return {
+  "nvim-mini/mini.trailspace",
+  event = "VeryLazy",
+  config = function()
+    require("mini.trailspace").setup()
+
+    -- Auto-trim trailing whitespace on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+        require("mini.trailspace").trim()
+      end,
+    })
+  end,
+}
