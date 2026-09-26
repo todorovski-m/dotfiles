81e6f16a407494061585d8a78b98909b503a8c5b
Mirko Todorovski <todorovski.mrk@gmail.com>
2026-09-26 12:54:24 +0200

    Add Mason tool dependencies

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "tinymist",
+        "typstyle",
+
+        "pyright",
+        "debugpy",
+        "ruff",
+
+        "texlab",
+        "fortls",
+
+        "clang-format",
+
+        "lua-language-server",
+
+        "prettier",
+
+        "shfmt",
+        "stylua",
+
+        "markdownlint-cli2",
+        "markdown-toc",
+
+        "yamlfix",
+        "yamlfmt",
+      },
+    },
+  },
+}
